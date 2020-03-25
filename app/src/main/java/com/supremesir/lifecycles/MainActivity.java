cdb4aa594d0a725f8b7c3270b4cb3b327fd76ea9
Haofan Fang <haofanfang@gmail.com>
2020-03-25 10:37:28 +0800

    普通写法实现计时器在后台时不计时

--- app/src/main/java/com/supremesir/lifecycles/MainActivity.java
+++ app/src/main/java/com/supremesir/lifecycles/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Chronometer;
 public class MainActivity extends AppCompatActivity {
 
     Chronometer chronometer;
+    private long elapsedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +21,32 @@ public class MainActivity extends AppCompatActivity {
 //        chronometer.setBase(System.currentTimeMillis());
         // 手机从上次启动到现在经过的毫秒数，一般用作时间段的统计
         chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.start();
     }
 
-    // 在视图层面的stop和start只是暂停和启动了chronometer的刷新效果
-    // chronometer的实际功能并未停止
     @Override
     protected void onPause() {
         super.onPause();
-        chronometer.stop();
+        elapsedTime = SystemClock.elapsedRealtime() - chronometer.getBase();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        chronometer.start();
+        chronometer.setBase(SystemClock.elapsedRealtime() - elapsedTime);
     }
+
+    // 在视图层面的stop和start只是暂停和启动了chronometer的刷新效果
+    // chronometer的实际功能并未停止
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        chronometer.stop();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        chronometer.start();
+//    }
 }
