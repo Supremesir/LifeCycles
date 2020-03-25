5cd63e1858e1dd3f9d177e733e1f2d0bafa17585
Haofan Fang <haofanfang@gmail.com>
2020-03-25 11:54:24 +0800

    用LifeCycle的方式实现

--- app/src/main/java/com/supremesir/lifecycles/MainActivity.java
+++ app/src/main/java/com/supremesir/lifecycles/MainActivity.java
@@ -1,15 +1,12 @@
 package com.supremesir.lifecycles;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.widget.Chronometer;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Chronometer chronometer;
-    private long elapsedTime;
+    MyChronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         chronometer = findViewById(R.id.meter);
-        // UNIX时间，从1970-1-1到现在经过的毫秒数
-//        chronometer.setBase(System.currentTimeMillis());
-        // 手机从上次启动到现在经过的毫秒数，一般用作时间段的统计
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        chronometer.start();
+        getLifecycle().addObserver(chronometer);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        elapsedTime = SystemClock.elapsedRealtime() - chronometer.getBase();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        chronometer.setBase(SystemClock.elapsedRealtime() - elapsedTime);
-    }
-
-    // 在视图层面的stop和start只是暂停和启动了chronometer的刷新效果
-    // chronometer的实际功能并未停止
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        chronometer.stop();
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        chronometer.start();
-//    }
 }
---
+++ app/src/main/java/com/supremesir/lifecycles/MyChronometer.java
@@ -0,0 +1,36 @@
+package com.supremesir.lifecycles;
+
+import android.content.Context;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.widget.Chronometer;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
+/**
+ * @author HaoFan Fang
+ * @date 2020/3/25 10:38
+ */
+
+public class MyChronometer extends Chronometer implements LifecycleObserver {
+
+    private long elapsedTime;
+
+    public MyChronometer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private void pauseMeter() {
+        elapsedTime = SystemClock.elapsedRealtime() - getBase();
+        stop();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private void resumeMeter() {
+        setBase(SystemClock.elapsedRealtime() - elapsedTime);
+        start();
+    }
+}
